f148e1ef429d6ca9a33b9d24b6b22c2c6dbb6b61
Campbell <cam@CAMPBELLs-MacBook-2.local>
2018-01-28 07:54:22 +0000

    Initial commit

---
+++ src/main/java/com/ccpp/aprplay/AprPlayApplication.java
@@ -0,0 +1,15 @@
+package com.ccpp.aprplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class AprPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprPlayApplication.class, args);
+	}
+}

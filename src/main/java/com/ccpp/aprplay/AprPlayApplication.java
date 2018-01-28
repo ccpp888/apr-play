e16116edb19ab97e9207da60e596127b3c057ef7
cam <ccpurvis@gmail.com>
2018-01-28 09:55:32 +0000

    Init: add Javadoc

--- src/main/java/com/ccpp/aprplay/AprPlayApplication.java
+++ src/main/java/com/ccpp/aprplay/AprPlayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *
+ * Application boot and entry
  */
 @SpringBootApplication
 public class AprPlayApplication {
